af8c3f3e7fa2ca1062ef695b734f64fb278a77b8
Mohamed-Ayman01 <lord39597@gmail.com>
2023-08-28 22:39:10 +0400

    made play, stop features

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
+let textArea = document.getElementById("speech");
+let playBtn = document.querySelector(".options .play")
+let pauseBtn = document.querySelector(".options .pause")
+let stopBtn = document.querySelector(".options .stop");
+
 let speechUtterance = new SpeechSynthesisUtterance()
 
-console.log(speechUtterance)
-console.log(speechSynthesis)
+playBtn.addEventListener("click", () => {
+  speechUtterance.text = textArea.value;
+
+  speechSynthesis.speak(speechUtterance);
+  
+  textArea.disabled = true;
+})
+
+speechUtterance.onend = () => {
+  textArea.disabled = false;
+}
+
+stopBtn.addEventListener("click", () => {
+  speechSynthesis.cancel();
+
+  textArea.disabled = false;
+});
+
+// console.log(speechUtterance)
+// console.log(speechSynthesis)
