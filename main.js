d85e13b4e9a15898db111aff5b2fe3f10abfa918
Mohamed-Ayman01 <lord39597@gmail.com>
2023-08-29 23:29:06 +0400

    coded pause btn , rate speed input + some fixes

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ let textArea = document.getElementById("speech");
 let playBtn = document.querySelector(".options .play")
 let pauseBtn = document.querySelector(".options .pause")
 let stopBtn = document.querySelector(".options .stop");
+let speedInput = document.querySelector("input#speed");
 
 let speechUtterance = new SpeechSynthesisUtterance()
 
 playBtn.addEventListener("click", () => {
-  speechUtterance.text = textArea.value;
-
-  speechSynthesis.speak(speechUtterance);
-  
-  textArea.disabled = true;
+  if (speechSynthesis.paused) {
+    speechSynthesis.paused = false;
+    speechSynthesis.resume();
+  } else {
+    speechUtterance.text = textArea.value;
+    
+    speechSynthesis.speak(speechUtterance);
+    
+    textArea.disabled = true;
+  }
 })
 
 speechUtterance.onend = () => {
@@ -23,5 +29,10 @@ stopBtn.addEventListener("click", () => {
   textArea.disabled = false;
 });
 
-// console.log(speechUtterance)
-// console.log(speechSynthesis)
+pauseBtn.addEventListener("click", () => {
+  if (speechSynthesis.speaking) speechSynthesis.pause();
+});
+
+speedInput.addEventListener("change", function () {
+  speechUtterance.rate = this.value;
+})
